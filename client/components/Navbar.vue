fdacd222b04cb9436a72c6b88423db1bf8f50712
irfan-arrosid <irfanarrosid4@gmail.com>
2023-08-15 23:24:54 +0700

    Style(client): Create hero section

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -1,5 +1,16 @@
 <template>
-    <div class="flex items-center" style="height: 70px">
-        <div class="logo text-primary font-bold text-2xl">Modateng</div>
+    <div class="flex items-center h-[70px]">
+        <NuxtLink class="logo text-primary font-bold text-2xl" to="/">Modateng</NuxtLink>
+        <ul class="flex ml-auto items-center gap-5">
+            <li>
+                <NuxtLink class="font-medium text-black hover:text-primary" to="/login">Log In</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink class="rounded-lg bg-secondary hover:bg-secondary-hover font-medium text-white px-5 py-3"
+                    to="/register">Sign
+                    Up
+                </NuxtLink>
+            </li>
+        </ul>
     </div>
 </template>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,7 +1,20 @@
 <template>
-    <section class="header border border-black">
+    <section class="header border border-black px-2.5 sm:px-5 lg:px-10">
         <div class="container mx-auto relative">
             <Navbar />
+        </div>
+    </section>
+    <section class="hero bg-primary px-2.5 sm:px-5 lg:px-10">
+        <div class="container mx-auto relative">
+            <div class="flex flex-col justify-center h-[375px] sm:h-[300px] lg:h-[575px]">
+                <h1 class="text-white font-bold text-4xl lg:text-5xl">Your Gateway to<br />Eventful Adventures</h1>
+                <div class="button mt-10">
+                    <NuxtLink class="rounded-lg bg-secondary hover:bg-secondary-hover font-medium text-white px-5 py-3"
+                        to="#">
+                        Start Explore
+                    </NuxtLink>
+                </div>
+            </div>
         </div>
     </section>
 </template>
