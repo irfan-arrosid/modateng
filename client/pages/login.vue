392f3529466d7b8d03eeebf095f7e3701a6f83a1
irfan-arrosid <irfanarrosid4@gmail.com>
2023-08-24 23:41:04 +0700

    Style(client): Create log-in page

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -2,13 +2,9 @@
     <div class="h-screen flex justify-center">
         <div class="bg-black w-1/3 hidden lg:block"></div>
         <div class="w-auto lg:w-2/3 flex justify-center items-center px-2.5 sm:px-5 lg:px-10">
-            <div class="w-full max-w-sm">
+            <div class="w-[384px] lg:w-full max-w-sm">
                 <NuxtLink class="logo text-primary font-bold text-2xl" to="/">Modateng</NuxtLink>
-                <div class="text-black font-bold text-5xl mt-8">Create an account</div>
-                <div class="name-field mt-8">
-                    <label for="">Name</label>
-                    <input type="text" class="border border-light-gray mt-2 block w-full rounded-lg px-6 py-3">
-                </div>
+                <div class="text-black font-bold text-5xl mt-8">Log in</div>
                 <div class="email-field mt-4">
                     <label for="">Email</label>
                     <input type="text" class="border border-light-gray mt-2 block w-full rounded-lg px-6 py-3">
@@ -17,20 +13,23 @@
                     <label for="">Password</label>
                     <input type="password" class="border border-light-gray mt-2 block w-full rounded-lg px-6 py-3">
                 </div>
-                <div class="confirm-password-field mt-4">
-                    <label for="">Confirm password</label>
-                    <input type="password" class="border border-light-gray mt-2 block w-full rounded-lg px-6 py-3">
+                <div class="flex justify-between mt-2 text-sm text-light-gray">
+                    <div class="flex gap-2">
+                        <input type="checkbox">
+                        <label for="">Remember me</label>
+                    </div>
+                    <NuxtLink to="#" class="underline">Forgot password?</NuxtLink>
                 </div>
                 <div class="button block mt-8 text-center">
                     <NuxtLink
                         class="bg-secondary hover:bg-secondary-hover rounded-lg px-5 py-3 block w-full text-white font-medium"
                         to="#">
-                        Create account
+                        Log in
                     </NuxtLink>
                 </div>
                 <div class="text-center mt-3">
-                    <div class="text-black text-sm">Already have account?
-                        <NuxtLink class="text-secondary" to="/login">Log in</NuxtLink>
+                    <div class="text-black text-sm">Don't have an account?
+                        <NuxtLink class="text-secondary" to="/register">Create now</NuxtLink>
                     </div>
                 </div>
             </div>
