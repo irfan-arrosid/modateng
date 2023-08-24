146b68f4370e5720751b8d1ac1251589aac7d016
irfan-arrosid <irfanarrosid4@gmail.com>
2023-08-24 23:56:18 +0700

    Style(client): Fix CTA button in hero section

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -4,8 +4,8 @@
         <div class="container mx-auto relative">
             <div class="flex flex-col justify-center h-[375px] sm:h-[300px] lg:h-[575px]">
                 <h1 class="text-white font-bold text-4xl lg:text-5xl">Your Gateway to<br />Eventful Adventures</h1>
-                <div class="button mt-10">
-                    <NuxtLink class="rounded-lg bg-primary hover:bg-primary-hover font-medium text-white px-5 py-3" to="#">
+                <div class="button mt-8 flex">
+                    <NuxtLink class="bg-primary hover:bg-primary-hover rounded-lg px-5 py-3 text-white font-medium" to="#">
                         Start Explore
                     </NuxtLink>
                 </div>
