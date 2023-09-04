9c3d519740382a79842010a05153bec49765fca8
irfan-arrosid <irfanarrosid4@gmail.com>
2023-09-04 21:37:49 +0700

    Style(client): Fix register page media responsivity

--- client/pages/register.vue
+++ client/pages/register.vue
@@ -1,33 +1,29 @@
 <template>
     <div class="h-screen flex justify-center">
-        <div class="bg-neutral w-1/3 hidden lg:block"></div>
-        <div class="w-auto lg:w-2/3 flex justify-center items-center px-2.5 sm:px-5 lg:px-10">
+        <div class="bg-neutral hidden lg:block lg:w-1/3"></div>
+        <div class="w-full lg:w-2/3 flex justify-center items-center px-2.5 sm:px-5 lg:px-10">
             <div class="w-full max-w-sm">
                 <NuxtLink class="text-primary font-bold text-2xl" to="/">Modateng</NuxtLink>
-                <div class="text-neutral font-bold text-5xl mt-8">Create an account</div>
-                <div class="form-control mt-8">
-                    <label class="label">Name</label>
-                    <input type="text" placeholder="" class="input input-bordered w-full" />
+                <div class="text-neutral font-bold text-5xl mt-6">Create an account</div>
+                <div class="form-control mt-6 sm:mt-8">
+                    <input type="text" placeholder="Name" class="input input-bordered w-full text-lg" />
                 </div>
-                <div class="form-control mt-2">
-                    <label class="label">Email</label>
-                    <input type="text" placeholder="" class="input input-bordered w-full" />
+                <div class="form-control mt-4">
+                    <input type="text" placeholder="Email" class="input input-bordered w-full text-lg" />
                 </div>
-                <div class="form-control mt-2">
-                    <label class="label">Password</label>
-                    <input type="password" placeholder="" class="input input-bordered w-full" />
+                <div class="form-control mt-4">
+                    <input type="password" placeholder="Password" class="input input-bordered w-full text-lg" />
                 </div>
-                <div class="form-control mt-2">
-                    <label class="label">Confirm password</label>
-                    <input type="password" placeholder="" class="input input-bordered w-full" />
+                <div class="form-control mt-4">
+                    <input type="password" placeholder="Confirm password" class="input input-bordered w-full text-lg" />
                 </div>
-                <div class="mt-8 text-center">
-                    <NuxtLink class="btn btn-secondary normal-case font-medium text-base text-base-100 w-full" to="#">
+                <div class="mt-6 sm:mt-8 text-center">
+                    <NuxtLink class="btn btn-secondary normal-case font-medium text-lg text-base-100 w-full" to="#">
                         Create account
                     </NuxtLink>
                 </div>
                 <div class="text-center mt-3">
-                    <div class="text-neutral text-sm">Already have account?
+                    <div class="text-neutral text-base">Already have account?
                         <NuxtLink class="text-secondary" to="/login">Log in</NuxtLink>
                     </div>
                 </div>
