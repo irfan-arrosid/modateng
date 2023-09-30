ebe5c46ee9dee10868c4545f1d859406264617e5
irfanarrosid <irfanarrosid4@gmail.com>
2023-09-30 22:29:11 +0700

    Feat: Create Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,46 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+
+export default async function Navbar() {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <div className="navbar h-[70px] px-2.5 sm:px-5 lg:px-10">
+      <Link className="logo text-primary font-bold text-2xl" href="/">
+        Modateng
+      </Link>
+      {session?.user ? (
+        <ul className="flex ml-auto items-center gap-2">
+          <li>
+            <Link
+              className="btn btn-secondary text-base-100 font-medium normal-case text-base"
+              href="/logout"
+            >
+              Log Out
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="flex ml-auto items-center gap-2">
+          <li>
+            <Link
+              className="btn btn-ghost text-black font-medium normal-case text-base"
+              href="/login"
+            >
+              Log In
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="btn btn-secondary text-base-100 font-medium normal-case text-base"
+              href="/register"
+            >
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+}
